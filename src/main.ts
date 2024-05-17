0a989008ae071d6d6904af574ca3ce54828165cf
Débora Gonçalves <debora.crishsm2@hotmail.com>
2024-05-17 19:08:18 +0100

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
+  app.enableCors();
   await app.listen(3001);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
